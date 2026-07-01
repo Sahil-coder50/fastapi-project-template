ef4d95923a39aedae78029cb7800364a4447aa39
Sahil-coder50 <sahilsheoran24@gmail.com>
2026-07-01 12:24:51 +0000

    feat: Added Docker Code

---
+++ {{cookiecutter.project_slug}}/docker/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+echo "Waiting for PostgreSQL..."
+
+while ! nc -z postgres 5432; do
+    sleep 1
+done
+
+echo "PostgreSQL is available."
+
+if [ "$RUN_MIGRATIONS" = "1" ]; then
+    echo "Running Alembic migrations..."
+    alembic revision --autogenerate
+    alembic upgrade head
+fi
+
+echo "Starting FastAPI..."
+
+exec "$@"
